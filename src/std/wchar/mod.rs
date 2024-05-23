3c6007ff4721395e51335848ec01f13d8fa71846
Veronika Mikhaylova <theexanori@gmail.com>
2024-05-23 16:04:57 +0300

    add wchar string manipulation routines
    Signed-off-by: Veronika Mikhaylova <theexanori@gmail.com>

--- src/std/wchar/mod.rs
+++ src/std/wchar/mod.rs
@@ -6,6 +6,9 @@ use {
   core::ptr
 };
 
+use crate::support::algorithm::twoway;
+use core::slice;
+
 #[no_mangle]
 pub extern "C" fn rs_wmemccpy(
   dest: *mut wchar_t,
@@ -142,3 +145,385 @@ pub extern "C" fn rs_wmemset(
   }
   s
 }
+
+#[no_mangle]
+pub extern "C" fn rs_wcpcpy(
+  dest: *mut wchar_t,
+  src: *const wchar_t
+) -> *mut wchar_t {
+  rs_wcpncpy(dest, src, rs_wcslen(src) + 1)
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcpncpy(
+  dest: *mut wchar_t,
+  src: *const wchar_t,
+  n: size_t
+) -> *mut wchar_t {
+  let mut i = n;
+  let mut d = dest;
+  let mut s = src;
+  unsafe {
+    while i > 0 && *s != 0 {
+      *d = *s;
+      d = d.offset(1);
+      s = s.offset(1);
+      i -= 1;
+    }
+  }
+  let end = d;
+  while i > 0 {
+    unsafe {
+      *d = 0;
+      d = d.offset(1);
+    }
+    i -= 1;
+  }
+  end
+}
+
+// do wcscasecmp
+
+#[no_mangle]
+pub extern "C" fn rs_wcscat(
+  dest: *mut wchar_t,
+  src: *const wchar_t
+) -> *mut wchar_t {
+  rs_wcsncat(dest, src, rs_wcslen(src) + 1)
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcschr(
+  s: *const wchar_t,
+  c: wchar_t
+) -> *mut wchar_t {
+  let mut s1 = s;
+  loop {
+    unsafe {
+      if *s1 == c {
+        return s1 as *mut wchar_t;
+      }
+      if *s1 == 0 {
+        return ptr::null_mut();
+      }
+      s1 = s1.offset(1);
+    }
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcscmp(
+  left: *const wchar_t,
+  right: *const wchar_t
+) -> c_int {
+  rs_wcsncmp(left, right, usize::MAX)
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcscoll(
+  s1: *const wchar_t,
+  s2: *const wchar_t
+) -> c_int {
+  rs_wcscmp(s1, s2)
+}
+
+// do wcscoll_l
+
+#[no_mangle]
+pub extern "C" fn rs_wcscpy(
+  dest: *mut wchar_t,
+  src: *const wchar_t
+) -> *mut wchar_t {
+  rs_wcsncpy(dest, src, rs_wcslen(src) + 1)
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcscspn(
+  src: *const wchar_t,
+  segment: *const wchar_t
+) -> size_t {
+  let mut s1 = src;
+  let mut s2 = segment;
+  let mut bitset = BitSet256::new();
+  let mut i = 0;
+  unsafe {
+    while *s2 != 0 {
+      bitset.insert(*s2 as usize);
+      s2 = s2.offset(1);
+    }
+    while *s1 != 0 && !bitset.contains(*s1 as usize) {
+      i += 1;
+      s1 = s1.offset(1);
+    }
+  }
+  i
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcslen(s: *const wchar_t) -> size_t {
+  let mut len: size_t = 0;
+  let mut s = s;
+  unsafe {
+    while *s != 0 {
+      s = s.offset(1);
+      len += 1;
+    }
+  }
+  len
+}
+
+// do wcsncasecmp
+
+#[no_mangle]
+pub extern "C" fn rs_wcsncat(
+  dest: *mut wchar_t,
+  src: *const wchar_t,
+  n: size_t
+) -> *mut wchar_t {
+  let mut i = n;
+
+  if n != 0 {
+    let mut d = dest;
+    let mut s = src;
+    unsafe {
+      while *d != 0 {
+        d = d.offset(1);
+      }
+    }
+    while i != 0 {
+      unsafe {
+        *d = *s;
+        s = s.offset(1);
+        if *d == 0 {
+          break;
+        }
+        d = d.offset(1);
+      }
+      i -= 1;
+    }
+    unsafe { *d = 0 };
+  }
+  dest
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcsncmp(
+  left: *const wchar_t,
+  right: *const wchar_t,
+  n: size_t
+) -> c_int {
+  let mut l = left;
+  let mut r = right;
+  let mut i = n;
+  while i != 0 {
+    unsafe {
+      let c1 = *l as wchar_t;
+      l = l.offset(1);
+      let c2 = *r as wchar_t;
+      r = r.offset(1);
+      if c1 != c2 {
+        return c1 as c_int - c2 as c_int;
+      }
+      if c1 == 0 {
+        break;
+      }
+    }
+    i -= 1;
+  }
+  0
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcsncpy(
+  dest: *mut wchar_t,
+  src: *const wchar_t,
+  n: size_t
+) -> *mut wchar_t {
+  let mut i = n;
+  let mut d = dest;
+  let mut s = src;
+  unsafe {
+    while i > 0 && *s != 0 {
+      *d = *s;
+      d = d.offset(1);
+      s = s.offset(1);
+      i -= 1;
+    }
+  }
+  while i != 0 {
+    unsafe {
+      *d = 0;
+      d = d.offset(1);
+    }
+    i -= 1;
+  }
+  dest
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcsnlen(
+  s: *const wchar_t,
+  n: size_t
+) -> size_t {
+  let mut i = 0;
+  while i < n {
+    unsafe {
+      if *s.add(i) == 0 {
+        break;
+      }
+    }
+    i += 1;
+  }
+  i as size_t
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcspbrk(
+  src: *const wchar_t,
+  breakset: *const wchar_t
+) -> *mut wchar_t {
+  let mut s1 = src;
+  let mut s2 = breakset;
+  let mut bitset = BitSet256::new();
+  unsafe {
+    while *s2 != 0 {
+      bitset.insert(*s2 as usize);
+      s2 = s2.offset(1);
+    }
+    while *s1 != 0 && !bitset.contains(*s1 as usize) {
+      s1 = s1.offset(1);
+    }
+    if *s1 != 0 { s1 as *mut wchar_t } else { ptr::null_mut() }
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcsrchr(
+  s: *const wchar_t,
+  c: wchar_t
+) -> *mut wchar_t {
+  let mut s1 = s;
+  let mut last = ptr::null_mut();
+  loop {
+    unsafe {
+      if *s1 == c {
+        last = s1 as *mut wchar_t;
+      }
+      if *s1 == 0 {
+        return last;
+      }
+      s1 = s1.offset(1);
+    }
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcsspn(
+  src: *const wchar_t,
+  segment: *const wchar_t
+) -> size_t {
+  let mut s1 = src;
+  let mut s2 = segment;
+  let mut bitset = BitSet256::new();
+  let mut i = 0;
+  unsafe {
+    while *s2 != 0 {
+      bitset.insert(*s2 as usize);
+      s2 = s2.offset(1);
+    }
+    while *s1 != 0 && bitset.contains(*s1 as usize) {
+      i += 1;
+      s1 = s1.offset(1);
+    }
+  }
+  i
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcsstr(
+  haystack: *const wchar_t,
+  needle: *const wchar_t
+) -> *mut wchar_t {
+  let nlen = rs_wcslen(needle);
+
+  if nlen == 0 {
+    return haystack.cast_mut();
+  }
+  if nlen == 1 {
+    unsafe { return rs_wcschr(haystack, *needle as wchar_t) };
+  }
+
+  let hlen = rs_wcslen(haystack);
+  let h = unsafe { slice::from_raw_parts(haystack, hlen) };
+  let n = unsafe { slice::from_raw_parts(needle, nlen) };
+  let compare = |left: *const wchar_t,
+                 right: *const wchar_t,
+                 n: size_t|
+   -> bool { rs_wmemcmp(left.cast(), right.cast(), n) == 0 };
+
+  let result = match twoway::twoway(h, n, compare) {
+    | Some(x) => x.as_ptr().cast_mut(),
+    | None => ptr::null_mut()
+  };
+  result
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcstok(
+  s: *mut wchar_t,
+  sep: *const wchar_t,
+  lasts: *mut *mut wchar_t
+) -> *mut wchar_t {
+  let mut s1 = s;
+  let mut sep1 = sep;
+  let mut bitset = BitSet256::new();
+  if s1.is_null() {
+    s1 = unsafe { *lasts };
+    if s1.is_null() {
+      return ptr::null_mut();
+    }
+  }
+  unsafe {
+    while *sep1 != 0 {
+      bitset.insert(*sep1 as usize);
+      sep1 = sep1.offset(1);
+    }
+    while *s1 != 0 && bitset.contains(*s1 as usize) {
+      s1 = s1.offset(1);
+    }
+    if *s1 == 0 {
+      *lasts = s1;
+      return ptr::null_mut();
+    }
+  }
+  let token = s1;
+  unsafe {
+    while *s1 != 0 {
+      if bitset.contains(*s1 as usize) {
+        *s1 = 0;
+        s1 = s1.offset(1);
+        break;
+      }
+      s1 = s1.offset(1);
+    }
+    *lasts = s1;
+  }
+  token
+}
+
+#[no_mangle]
+pub extern "C" fn rs_wcsxfrm(
+  s1: *mut wchar_t,
+  s2: *const wchar_t,
+  n: size_t
+) -> size_t {
+  let len = rs_wcslen(s2);
+  if len < n {
+    rs_wcsncpy(s1, s2, n);
+  }
+  len
+}
+
+// do wcsxfrm_l
+
+// Allocated memory stuff: strdup
--- tests/wchar.cc
+++ tests/wchar.cc
@@ -8,6 +8,32 @@ extern "C" {
   wchar_t *rs_wmemcpy(wchar_t *, const wchar_t *, size_t);
   wchar_t *rs_wmemmove(wchar_t *, const wchar_t *, size_t);
   wchar_t *rs_wmemset(wchar_t *, wchar_t, size_t);
+  wchar_t *rs_wcpcpy(wchar_t *, const wchar_t *);
+  wchar_t *rs_wcpncpy(wchar_t *, const wchar_t *, size_t);
+  //int rs_wcscasecmp(const wchar_t *, const wchar_t *);
+  //int rs_wcscasecmp_l(const wchar_t *, const wchar_t *, locale_t);
+  wchar_t *rs_wcscat(wchar_t *, const wchar_t *);
+  wchar_t *rs_wcschr(const wchar_t *, wchar_t);
+  int rs_wcscmp(const wchar_t *, const wchar_t *);
+  int rs_wcscoll(const wchar_t *, const wchar_t *);
+  int rs_wcscoll_l(const wchar_t *, const wchar_t *, locale_t);
+  wchar_t *rs_wcscpy(wchar_t *, const wchar_t *);
+  size_t rs_wcscspn(const wchar_t *, const wchar_t *);
+  wchar_t *rs_wcsdup(const wchar_t *);
+  size_t rs_wcslen(const wchar_t *);
+  //int rs_wcsncasecmp(const wchar_t *, const wchar_t *, size_t);
+  //int rs_wcsncasecmp_l(const wchar_t *, const wchar_t *, size_t, locale_t);
+  wchar_t *rs_wcsncat(wchar_t *, const wchar_t *, size_t);
+  int rs_wcsncmp(const wchar_t *, const wchar_t *, size_t);
+  wchar_t *rs_wcsncpy(wchar_t *, const wchar_t *, size_t);
+  size_t rs_wcsnlen(const wchar_t *, size_t);
+  wchar_t *rs_wcspbrk(const wchar_t *, const wchar_t *);
+  wchar_t *rs_wcsrchr(const wchar_t *, wchar_t);
+  size_t rs_wcsspn(const wchar_t *, const wchar_t *);
+  wchar_t *rs_wcsstr(const wchar_t *, const wchar_t *);
+  wchar_t *rs_wcstok(wchar_t *, const wchar_t *, wchar_t **);
+  size_t rs_wcsxfrm(wchar_t *, const wchar_t *, size_t);
+  size_t rs_wcsxfrm_l(wchar_t *, const wchar_t *, size_t, locale_t);
 }
 
 TEST(wmemchr, null) {
@@ -72,4 +98,160 @@ TEST(wmemset, example) {
   ASSERT_EQ(buf, rs_wmemset(buf, L'!', 10));
   buf[10] = L'\0';
   ASSERT_STREQ(L"!!!!!!!!!!", buf);
+}
+
+TEST(wcpcpy, example) {
+  wchar_t buf[] = L"AAAAAAAAAA";
+  ASSERT_EQ(buf, rs_wcpcpy(buf, L""));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"\0AAAAAAAAA"));
+  ASSERT_EQ(buf + 5, rs_wcpcpy(buf, L"Hello"));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"Hello\0AAAA"));
+  ASSERT_EQ(buf, rs_wcpcpy(buf, L""));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"\0ello\0AAAA"));
+  ASSERT_EQ(buf + 9, rs_wcpcpy(buf, L"Example!!"));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"Example!!\0"));
+}
+
+TEST(wcpncpy, null) {
+  ASSERT_EQ((wchar_t *)12, rs_wcpncpy((wchar_t *)12, (wchar_t *)500, 0));
+}
+
+TEST(wcpncpy, example1) {
+  wchar_t buf[] = L"AAAAAAAAAAAA";
+  ASSERT_EQ(buf + 5, rs_wcpncpy(buf, L"Hello", 12));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"Hello\0\0\0\0\0\0\0"));
+}
+
+TEST(wcpncpy, example2) {
+  wchar_t buf[13];
+  ASSERT_EQ(buf + 12, rs_wcpncpy(buf, L"This is a very long string", 12));
+  buf[12] = '\0';
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"This is a ve"));
+}
+
+TEST(wcscat, example) {
+  wchar_t buf[] = L"\0AAAAAAAAA";
+  ASSERT_EQ(buf, rs_wcscat(buf, L""));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"\0AAAAAAAAA"));
+  ASSERT_EQ(buf, rs_wcscat(buf, L"Hello"));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"Hello\0AAAA"));
+  ASSERT_EQ(buf, rs_wcscat(buf, L""));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"Hello\0AAAA"));
+  ASSERT_EQ(buf, rs_wcscat(buf, L"!!!!"));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"Hello!!!!\0"));
+}
+
+TEST(wcscpy, example) {
+  wchar_t buf[] = L"AAAAAAAAAA";
+  ASSERT_EQ(buf, rs_wcscpy(buf, L""));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"\0AAAAAAAAA"));
+  ASSERT_EQ(buf, rs_wcscpy(buf, L"Hello"));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"Hello\0AAAA"));
+  ASSERT_EQ(buf, rs_wcscpy(buf, L""));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"\0ello\0AAAA"));
+  ASSERT_EQ(buf, rs_wcscpy(buf, L"Example!!"));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"Example!!\0"));
+}
+
+TEST(wcscspn, example) {
+  const wchar_t *wcs = L"Hello, world";
+  ASSERT_EQ(0, rs_wcscspn(wcs, L"H"));
+  ASSERT_EQ(7, rs_wcscspn(wcs, L"rdw"));
+  ASSERT_EQ(12, rs_wcscspn(wcs, L"XYZ"));
+}
+
+TEST(wcslen, example) {
+  ASSERT_EQ(22, rs_wcslen(L"ℕ ⊆ ℕ₀ ⊂ ℤ ⊂ ℚ ⊂ ℝ ⊂ ℂ"));
+}
+
+TEST(wcsncat, example) {
+  wchar_t buf[11] = L"\0AAAAAAAAA";
+  ASSERT_EQ(buf, rs_wcsncat(buf, L"", 0));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"\0AAAAAAAAA"));
+  ASSERT_EQ(buf, rs_wcsncat(buf, L"Hello", 99999));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"Hello\0AAAA"));
+  ASSERT_EQ(buf, rs_wcsncat(buf, L"", 1));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"Hello\0AAAA"));
+  ASSERT_EQ(buf, rs_wcsncat(buf, L"!!!!!!!!!!!!", 3));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"Hello!!!\0A"));
+}
+
+TEST(wcsncmp, null) {
+  ASSERT_EQ(0, rs_wcsncmp(NULL, NULL, 0));
+}
+
+TEST(wcsncmp, examples) {
+  ASSERT_EQ(0, rs_wcsncmp(L"", L"", 100));
+  ASSERT_EQ(0, rs_wcsncmp(L"Hello", L"Hello", 100));
+
+  ASSERT_EQ(0, rs_wcsncmp(L"Hello", L"Hello, world", 5));
+  ASSERT_GT(0, rs_wcsncmp(L"Hello", L"Hello, world", 6));
+  ASSERT_LT(0, rs_wcsncmp(L"Hello, world", L"Hello", 100));
+
+  ASSERT_EQ(0, rs_wcsncmp(L"Hello!", L"Hello.", 5));
+  ASSERT_GT(0, rs_wcsncmp(L"Hello!", L"Hello.", 6));
+  ASSERT_LT(0, rs_wcsncmp(L"Hello.", L"Hello!", 100));
+}
+
+TEST(wcsncpy, null) {
+  ASSERT_EQ((wchar_t *)12, rs_wcsncpy((wchar_t *)12, (wchar_t *)500, 0));
+}
+
+TEST(wcsncpy, example1) {
+  wchar_t buf[] = L"AAAAAAAAAAAA";
+  ASSERT_EQ(buf, rs_wcsncpy(buf, L"Hello", 12));
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"Hello\0\0\0\0\0\0\0"));
+}
+
+TEST(wcsncpy, example2) {
+  wchar_t buf[13];
+  ASSERT_EQ(buf, rs_wcsncpy(buf, L"This is a very long string", 12));
+  buf[12] = L'\0';
+  ASSERT_THAT(buf, testing::ElementsAreArray(L"This is a ve"));
+}
+
+
+TEST(wcsnlen, null) {
+  ASSERT_EQ(0, rs_wcsnlen(NULL, 0));
+  ASSERT_EQ(0, rs_wcsnlen(L"", 100));
+  ASSERT_EQ(7, rs_wcsnlen(L"Σὲ γνωρίζω ἀπὸ τὴν κόψη", 7));
+}
+
+TEST(wcspbrk, example) {
+  const wchar_t *wcs = L"Hello, world";
+  ASSERT_EQ(wcs, rs_wcspbrk(wcs, L"H"));
+  ASSERT_EQ(wcs + 7, rs_wcspbrk(wcs, L"rdw"));
+  ASSERT_EQ(NULL, rs_wcspbrk(wcs, L"XYZ"));
+}
+
+TEST(wcsspn, example) {
+  const wchar_t *wcs = L"Hello, world";
+  ASSERT_EQ(0, rs_wcsspn(wcs, L""));
+  ASSERT_EQ(0, rs_wcsspn(wcs, L"Foo"));
+  ASSERT_EQ(5, rs_wcsspn(wcs, L"olHe"));
+  ASSERT_EQ(12, rs_wcsspn(wcs, L"Helo, wrld"));
+}
+
+TEST(wcsstr, examples) {
+  const wchar_t *str = (const wchar_t *)0x42;
+  ASSERT_EQ(str, rs_wcsstr(str, L""));
+
+  str = L"Hello world";
+  ASSERT_EQ(str + 2, rs_wcsstr(str, L"ll"));
+  ASSERT_EQ(str + 4, rs_wcsstr(str, L"o worl"));
+  ASSERT_EQ(str + 6, rs_wcsstr(str, L"world"));
+  ASSERT_EQ(str + 10, rs_wcsstr(str, L"d"));
+  ASSERT_EQ(NULL, rs_wcsstr(str, L"word"));
+  ASSERT_EQ(NULL, rs_wcsstr(str, L"world!"));
+}
+
+TEST(wcstok, example) {
+  wchar_t line[] = L"LINE  TO BE\t\tSEPARATED\n";
+  const wchar_t *split = L" \t\n";
+  wchar_t *lastws;
+  ASSERT_STREQ(L"LINE", rs_wcstok(line, split, &lastws));
+  ASSERT_STREQ(L"TO", rs_wcstok(NULL, split, &lastws));
+  ASSERT_STREQ(L"BE", rs_wcstok(NULL, split, &lastws));
+  ASSERT_STREQ(L"SEPARATED", rs_wcstok(NULL, split, &lastws));
+  ASSERT_EQ(NULL, rs_wcstok(NULL, split, &lastws));
 }
